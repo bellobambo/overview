f5237e078df07011fcc5fe4396ae35641acdce98
Mystic-esque <mysticx404@gmail.com>
2026-09-17 04:36:29 +0100

    fix: remove stale dist/ from git, add to .gitignore

--- dist/types/database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// ============================================
-// Database row types (match Supabase table schemas)
-// ============================================
-Object.defineProperty(exports, "__esModule", { value: true });
